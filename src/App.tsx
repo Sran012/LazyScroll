c919b590d248b7e4c4f0c9942a0b182decd9dfe5
Sran012 <88543485+Sran012@users.noreply.github.com>
2026-01-21 17:59:12 +0000

    initial commit by sujal 🥷

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <p className="text-4xl font-bold text-red-500">Hello Sujal, this is your worst version saying hii to you !</p>
+    </>
+  )
+}
+
+export default App
